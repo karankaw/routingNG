613ee7e1d755936d509b2d966980b2c79914d1df
Kaw <703250313@genpact.com>
2021-04-22 01:06:02 +0530

    Committed href routerlink

--- routing-demo/src/app/app-routing.module.ts
+++ routing-demo/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DepartmentListComponent } from "./department-list/department-list.component";
+import { EmployeeListComponent } from "./employee-list/employee-list.component"
 
-
+const routes: Routes = [
+  {
+    path: "departments",
+    component: DepartmentListComponent
+  },
+  {
+    path: "employees",
+    component: EmployeeListComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents :any[] = [DepartmentListComponent, EmployeeListComponent]
--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
+// import { DepartmentListComponent } from './department-list/department-list.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+// import { routingComponents } from "./app-routing.module"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // DepartmentListComponent,
+    // EmployeeListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
